9298bd2ef02bbee91224337d6f3f5dd891d7175e
aleksandr_gvozdev <wayk5i@yandex.ru>
2025-05-20 22:17:12 +0300

    feat(Order): добавил функциональность чекбоксам

--- src/components/blocks/Order/Order.jsx
+++ src/components/blocks/Order/Order.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyledOrder } from "./style";
 import Form from "@layout/Form/Form";
 import Slider from "@layout/Slider/Slider";
 
-function Order({ products }) {    
+function Order({ products }) {
+    const defaultSelected = ["chicken-thigh", "goose-thigh", "beef-thigh"];
+    
+    const [selected, setSelected] = useState(() =>
+        defaultSelected.reduce((acc, p) => {
+            acc[p] = true;
+            return acc;
+        }, {})
+    );
+    
+    const selectedSlug = Object.entries(selected).filter(([, checked]) => checked).map(([slug]) => slug);
+        
+    const visibleProducts = products.filter((product) => selectedSlug.includes(product.slug));
+    
     return (
         <StyledOrder>
-            <Form />
-            <Slider products={products} />
+            <Form products={products} selected={selected} onSelectedChange={setSelected} />
+            <Slider products={visibleProducts} />
         </StyledOrder>
     );
 }
--- src/components/layout/Form/Form.jsx
+++ src/components/layout/Form/Form.jsx
@@ -1,51 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyledForm, StyledFieldSet, StyledLegend } from "./style";
 import ProductCheckBox from "@ui/ProductCheckBox/ProductCheckBox";
 import TextInput from "@ui/TextInput/TextInput";
 import Button from "@ui/Button/Button";
 import Price from "@ui/Price/Price";
 
-const products = [
-    "Филе бедра цыпленка",
-    "Филе бедра гуся",
-    "Мякоть бедра говяжья",
-    "Грудка цыпленка на кости",
-    "Голень цыпленка"
-];
-
-const defaultSelected = ["Филе бедра цыпленка", "Филе бедра гуся", "Мякоть бедра говяжья"]
-
-function Form() {
-    const [selected, setSelected] = useState(() =>
-        defaultSelected.reduce((acc, item) => {
-            acc[item] = true;
-            return acc;
-        }, {})
-    );
-
+function Form({ products, selected, onSelectedChange }) {
     const handleChange = (e) => {
         const { value, checked } = e.target;
-        setSelected((prev) => ({
+        onSelectedChange((prev) => ({
         ...prev,
         [value]: checked,
         }));
     };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Выбранные продукты:", selected);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Выбранные продукты:", selected);
+    };
 
     return (
         <StyledForm onSubmit={handleSubmit}>
             <StyledFieldSet>
                 <StyledLegend>Выберите продукты</StyledLegend>                
                 {products.map((product) => (
-                    <ProductCheckBox key={product}
-                        label={product}
+                    <ProductCheckBox 
+                        key={product.slug}
+                        label={product.name}
                         name="product"
-                        value={product}
-                        checked={!!selected[product]}
+                        value={product.slug}
+                        checked={!!selected[product.slug]}
                         onChange={handleChange}
                 />
                 ))}                
