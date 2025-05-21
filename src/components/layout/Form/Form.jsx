ccad6b7013206829346fcaca7b8ea8657ccc6f43
aleksandr_gvozdev <wayk5i@yandex.ru>
2025-05-21 10:46:47 +0300

    feat(Form): собрал и вывел инфу о заказе

--- src/components/layout/Form/Form.jsx
+++ src/components/layout/Form/Form.jsx
@@ -1,56 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyledForm, StyledFieldSet, StyledLegend, SubmitButton } from "./style";
 import ProductCheckBox from "@ui/ProductCheckBox/ProductCheckBox";
 import TextInput from "@ui/TextInput/TextInput";
 import Price from "@ui/Price/Price";
 
 function Form({ products, selected, onSelectedChange, selectedSlug }) {
+    const [email, setEmail] = useState("");
+
     const handleChange = (e) => {
-        const { value, checked } = e.target;
-        onSelectedChange((prev) => ({
-        ...prev,
-        [value]: checked,
-        }));
+        const { name, value, checked, type } = e.target;
+
+        if (type === "checkbox") {
+            onSelectedChange((prev) => ({
+                ...prev,
+                [value]: checked,
+            }));
+        } else if (name === "mail") {
+            setEmail(value);
+        }
+
     };
 
+    const isValidEmail = (email) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.trim());
+    };
+    const isFormValid = isValidEmail(email) && selectedSlug.length > 0;
+
     const totalPrice = products.filter((product) => selectedSlug.includes(product.slug))
-                       .reduce((sum, product) => {
-                            const match = product.price.match(/^(\d+)/);
-                            const numericPrice = match ? parseInt(match[1], 10) : 0;
-                            return sum + numericPrice;
-                       }, 0);
+        .reduce((sum, product) => {
+            const match = product.price.match(/^(\d+)/);
+            const numericPrice = match ? parseInt(match[1], 10) : 0;
+            return sum + numericPrice;
+        }, 0);
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const selectedProducts = Object.entries(selected).filter(([, checked]) => checked).map(([slug]) => slug);        
-        console.log("Выбранные продукты:", selectedProducts);
+        const selectedProducts = Object.entries(selected).filter(([, checked]) => checked).map(([slug]) => slug);
+
+        const message = `
+                Электронная почта: ${email}
+
+                Выбранные продукты:
+                ${selectedProducts.join(", ")}
+
+                Цена заказа: ${totalPrice} руб.
+            `;
+
+        alert(message);
     };
 
     return (
         <StyledForm onSubmit={handleSubmit}>
             <StyledFieldSet>
-                <StyledLegend>Выберите продукты</StyledLegend>                
+                <StyledLegend>Выберите продукты</StyledLegend>
                 {products.map((product) => (
-                    <ProductCheckBox 
+                    <ProductCheckBox
                         key={product.slug}
                         label={product.name}
                         name="product"
                         value={product.slug}
                         checked={!!selected[product.slug]}
                         onChange={handleChange}
-                />
-                ))}                
+                    />
+                ))}
             </StyledFieldSet>
             <StyledFieldSet>
                 <StyledLegend>Сделать заказ</StyledLegend>
                 <TextInput
                     name="mail"
-                    placeholder="Введите адрес доставки"                    
+                    type="email"
+                    placeholder="Введите адрес доставки"
                     onChange={handleChange}
                 />
                 <Price total={totalPrice} />
-                <SubmitButton type="submit">Купить</SubmitButton>
+                <SubmitButton type="submit" disabled={!isFormValid}>Купить</SubmitButton>
             </StyledFieldSet>
         </StyledForm>
     );
